1363f618a1594b614f05f8e29c0972f153339dde
Mohsin Imran <Mohsin-Imran-dev>
2026-02-23 23:46:01 +0500

    Remove useNewUrlParser/useUnifiedTopology for Mongoose 7+

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // add at the very top
+
 const express = require("express");
 const path = require("path");
 const storeRouter = require("./routes/storeRouter");
@@ -7,19 +9,26 @@ const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
-const DB_PATH =
-  "mongodb+srv://root:7272@mcoding.9qq795b.mongodb.net/airbnb?appName=mCoding";
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+const MONGO_URL = process.env.MONGO_URL; // environment variable
+
+if (!MONGO_URL) {
+  throw new Error("MongoDB connection string is missing. Set MONGO_URL in .env or Railway Variables");
+}
+
 const store = new MongoDBStore({
-  uri: DB_PATH,
+  uri: MONGO_URL,      // use env variable here too
   collection: "sessions",
 });
+
 app.use(express.static(path.join(rootDir, "public")));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     secret: "Knowledge Gate AI",
@@ -28,41 +37,39 @@ app.use(
     store: store,
   }),
 );
+
 app.use((req, res, next) => {
   res.locals.isLoggedIn = req.session.isLoggedIn;
   res.locals.userType = req.session.user ? req.session.user.userType : null;
   next();
 });
+
 app.use((req, res, next) => {
   req.isLoggedIn = req.session.isLoggedIn;
   next();
 });
+
 app.use(storeRouter);
 app.use(authRouter);
 
 app.use("/host", (req, res, next) => {
-  if (req.isLoggedIn) {
-    next();
-  } else {
-    res.redirect("/login");
-  }
+  if (req.isLoggedIn) next();
+  else res.redirect("/login");
 });
+
 app.use("/host", hostRouter);
 app.use(errorsController.pageNotFound);
 
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGO_URL)
   .then(() => {
     console.log("Connected to mongo");
     app.listen(PORT, () => {
-      console.log(`Server listening on address http://localhost:${PORT}`);
+      console.log(`Server listening on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log("Error while connecting to mongoose: " + err);
+    console.log("Error while connecting to mongoose:", err);
   });
